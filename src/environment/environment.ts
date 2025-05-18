43b862fee0260ab695b78406c4bf2190c3bfa014
Tanatos <cristian_alessandria@hotmail.com>
2025-05-18 00:01:34 -0300

    Inicio de proyecto

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDgdJGB4bIueBFCpvWK2d0HB40GJi-as4o",
+        authDomain: "storejcexclusivo.firebaseapp.com",
+        projectId: "storejcexclusivo",
+        storageBucket: "storejcexclusivo.firebasestorage.app",
+        messagingSenderId: "740660291155",
+        appId: "1:740660291155:web:8c9f5bf764ab98606488a6"
+    },
+  };  
