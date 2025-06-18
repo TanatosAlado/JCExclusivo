7a94628f76b16af18fad764aa44ed940bdd9298b
[esterotech] <[infoesterotech@gmail.com]>
2025-06-18 15:46:28 -0300

    niv main

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LoginComponent } from './views/login/login.component';
+import { RegistroComponent } from './views/registro/registro.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegistroComponent
+  ],
   imports: [
     CommonModule,
     SharedModule
