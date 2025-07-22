737c361b9b3382adce4728f9d0f5707a28d7a494
[esterotech] <[infoesterotech@gmail.com]>
2025-07-22 09:29:53 -0300

    subiendo css productos

--- src/app/modules/admin/views/productos/components/lista-productos/lista-productos.component.ts
+++ src/app/modules/admin/views/productos/components/lista-productos/lista-productos.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AltaProductoComponent } from '../alta-producto/alta-producto.component';
 import { ProductosService } from 'src/app/modules/shop/services/productos.service';
 import { Producto } from 'src/app/modules/shop/models/producto.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { EdicionProductoComponent } from '../edicion-producto/edicion-producto.component';
+import { ConfirmDialogComponent } from 'src/app/shared/components/confirm-dialog/confirm-dialog.component';
+import { DetalleProductoComponent } from '../detalle-producto/detalle-producto.component';
 
 @Component({
   selector: 'app-lista-productos',
@@ -16,6 +21,9 @@ export class ListaProductosComponent {
   marcasUnicas: string[] = [];
   displayedColumns: string[] = ['nombre', 'descripcion', 'acciones'];
   productos: Producto[] = [];
+  datasourceProductos: MatTableDataSource<Producto>
+  paginator!: MatPaginator;
+  public productoAEliminar: string = ''; 
 
   constructor(public dialog: MatDialog, private productosService: ProductosService,){
 
@@ -25,6 +33,35 @@ export class ListaProductosComponent {
     this.obtenerProductos();
   }
 
+     ngAfterViewInit() {
+    this.setDataSourceAttributes()
+  }
+
+   @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
+      if (mp) {
+        this.paginator = mp;
+        this.paginator._intl.itemsPerPageLabel = 'Productos por Página';
+        this.paginator._intl.firstPageLabel = 'Primera Página';
+        this.paginator._intl.previousPageLabel = 'Página Anterior';
+        this.paginator._intl.nextPageLabel = 'Siguiente Página';
+        this.paginator._intl.lastPageLabel = 'Última Página';
+      }
+      this.setDataSourceAttributes();
+    }
+
+  setDataSourceAttributes() {
+    if (this.datasourceProductos) {
+      this.datasourceProductos.paginator = this.paginator;
+    }
+  }
+  obte
+
+       //FUNCION PARA FILTRAR POR CUALQUIER PALABRA QUE SE ESCRIBA EN EL FILTRO
+  applyFilter(event: Event, datasource: MatTableDataSource<any>) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    datasource.filter = filterValue.trim().toLowerCase();
+  }
+
     obtenerProductos(): void {
     this.productosService.obtenerProductos().subscribe((productos: Producto[]) => {
       this.productos = productos;
@@ -32,6 +69,8 @@ export class ListaProductosComponent {
       this.rubrosUnicos = [...new Set(productos.map(p => p.rubro.toUpperCase()))];
       this.subrubrosUnicos = [...new Set(productos.map(p => p.subrubro.toUpperCase()))];
       this.marcasUnicas = [...new Set(productos.map(p => p.marca.toUpperCase()))];
+      this.datasourceProductos = new MatTableDataSource(this.productos);
+      this.datasourceProductos.paginator = this.paginator;
     });
   }
 
@@ -57,12 +96,29 @@ export class ListaProductosComponent {
   });
 }
 
-  editarProducto(producto: Producto): void {
+editarProducto(producto: Producto): void {
+    const dialogRef = this.dialog.open(EdicionProductoComponent, {
+      width: '600px',
+      data: producto,  // Enviar los datos del producto a editar
+    });
 
+    dialogRef.afterClosed().subscribe((resultado) => {
+      if (resultado) {
+        this.productosService.actualizarProducto(resultado)
+        .then(() => {
+          this.obtenerProductos(); // Refrescar la lista después de cerrar el modal
+        })
+        .catch(error => {
+        });
+      }
+    });
   }
 
-    verProducto(producto: any): void {
-
+     verProducto(producto: any): void {
+    this.dialog.open(DetalleProductoComponent, {
+      width: '500px',
+      data: producto
+    });
   }
 
   eliminarProducto(id: string): void {
@@ -70,7 +126,14 @@ export class ListaProductosComponent {
   }
 
     openConfirmDialog(producto: any): void {
-
+    this.productoAEliminar = producto.id;
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '400px',
+      data: {
+        message: `¿Está seguro que desea eliminar este producto: ${producto.nombre}?`,
+        confirmAction: () => this.eliminarProducto(producto.id) // Acción a ejecutar si se confirma
+      }
+    });
   }
 
 }
