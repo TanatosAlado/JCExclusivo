f454f78d531ca07df2c0485732bd408f0dcf2a29
Tanatos <cristian_alessandria@hotmail.com>
2025-07-31 13:17:44 -0300

    Alta ordenes

--- src/app/modules/admin/views/taller/components/lista-ordenes/lista-ordenes.component.ts
+++ src/app/modules/admin/views/taller/components/lista-ordenes/lista-ordenes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AltaOrdenComponent } from '../alta-orden/alta-orden.component';
 
 @Component({
   selector: 'app-lista-ordenes',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class ListaOrdenesComponent {
 
+  constructor(private dialog: MatDialog) { }
+
+  
+
+    abrirModalAltaProducto(): void {
+    const dialogRef = this.dialog.open(AltaOrdenComponent, {
+      width: '90vw',
+      maxWidth: '600px',
+      height: 'auto',
+      maxHeight: '90vh',
+      panelClass: 'custom-dialog-container',
+      
+    });
+  
+    dialogRef.afterClosed().subscribe(resultado => {
+      if (resultado) {
+        console.log('Producto creado:', resultado);
+     //   this.obtenerProductos(); // Refrescar productos
+      }
+    });
+  }
+
 }
+
+
