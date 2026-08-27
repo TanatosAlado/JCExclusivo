e7a86441e808f990a828740ca55cebe37cf9f107
Andrés Hosch <andreshosch@hotmail.com>
2026-08-27 11:11:39 -0300

    bsuqueda productos x categoria

--- src/app/shared/components/card-categorias/card-categorias.component.ts
+++ src/app/shared/components/card-categorias/card-categorias.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card-categorias',
@@ -8,6 +9,8 @@ import { Component } from '@angular/core';
 export class CardCategoriasComponent {
 
   imagenesCategorias: string[] = [];
+
+  constructor(private router: Router) {}
 
   ngOnInit() { 
     this.cargarImagenesCategorias();
@@ -23,6 +26,7 @@ export class CardCategoriasComponent {
       'assets/imagenes/categorías/may_01.jpg',
       'assets/imagenes/categorías/may_02.jpg',
       'assets/imagenes/categorías/may_03.jpg',
+      'assets/imagenes/categorías/may_04.jpg',
       'assets/imagenes/categorías/mas.jpg'
     ];
   } else {
@@ -30,8 +34,68 @@ export class CardCategoriasComponent {
       'assets/imagenes/categorías/min_01.jpg',
       'assets/imagenes/categorías/min_02.jpg',
       'assets/imagenes/categorías/min_03.jpg',
+      'assets/imagenes/categorías/min_04.jpg',
+      'assets/imagenes/categorías/min_05.jpg',
       'assets/imagenes/categorías/mas.jpg'
     ];
   }
   }
+
+  seleccionarCategoria(imagen: string) {
+
+  const tipoCliente = localStorage.getItem('clienteActual');
+  const cliente = JSON.parse(tipoCliente);
+
+  let rubro = '';
+
+  if (cliente.esMayorista === true) {
+
+    if (imagen.includes('may_01')) {
+      rubro = 'Accesorios';
+    }
+
+    if (imagen.includes('may_02')) {
+      rubro = 'Herramientas';
+    }
+
+    if (imagen.includes('may_03')) {
+      rubro = 'Insumos';
+    }
+
+    if (imagen.includes('may_04')) {
+      rubro = 'Repuestos';
+    }
+
+  } else {
+
+    if (imagen.includes('min_01')) {
+      rubro = 'Auriculares';
+    }
+
+    if (imagen.includes('min_02')) {
+      rubro = 'Cargadores';
+    }
+
+    if (imagen.includes('min_03')) {
+      rubro = 'Fundas';
+    }
+
+    if (imagen.includes('min_04')) {
+      rubro = 'Baterías';
+    }
+
+    if (imagen.includes('min_05')) {
+      rubro = 'Protector Pantalla';
+    }
+
+  }
+
+  // IR AL COMPONENTE DE PRODUCTOS
+  this.router.navigate(['/productos'], {
+    queryParams: {
+      rubro: rubro
+    }
+  });
+
+}
 }
